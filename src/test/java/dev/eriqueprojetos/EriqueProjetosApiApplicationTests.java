2f63ce853b33a98db4635d156caff5efdfed78e6
EriqueRocha <eriquebit@gmail.com>
2024-01-17 15:34:16 -0300

    teste

--- src/test/java/dev/eriqueprojetos/EriqueProjetosApiApplicationTests.java
+++ src/test/java/dev/eriqueprojetos/EriqueProjetosApiApplicationTests.java
@@ -1,11 +1,10 @@
-package com.dicionarioespanholAPI;
+package dev.eriqueprojetos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EriqueProjetosApiApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
